9d4b6df867794cdd08081bd09ada0eaea001adf6
hc <hheennrryy@gmail.com>
2013-02-23 18:07:01 -0500

    test Peer _init

--- test/peer.js
+++ test/peer.js
@@ -49,11 +49,42 @@ describe('Peer', function() {
 
   it('inherits from EventEmitter', function() {
     expect(p instanceof EventEmitter).to.be.equal(true);
-  });
+  })
 
   it('_getId')
 
-  it('_init')
+ /*
+  * Opens socket for Peer
+  * and registers 'message', 'error', 'socket-error' listeners
+  */
+  it('_init', function() {
+    var callSocketStart = false;
+    var socketstartFn = Socket.prototype.start;
+    Socket.prototype.start = function() { callSocketStart = true };
+
+    p._init();
+    // socket created
+    expect(p._socket instanceof Socket).to.be.equal(true);
+    // test listeners
+    var callHandle = false, callAbort = false;
+    p._handleServerJSONMessage = function(data) { callHandle = data };
+    p._abort = function(type, msg) { callAbort = type + msg }
+    // 'message' listener calls _handleServerJSONMessage
+    p._socket.emit('message', 'hi');
+    expect(callHandle).to.be.equal('hi');
+    // 'error' listener calls _abort
+    p._socket.emit('error', 'err!!!');
+    expect(callAbort).to.be.equal('socket-errorerr!!!');
+    // 'close' listener calls _abort
+    p._socket.emit('close');
+    expect(callAbort).to.match(/^socket-closed/);
+
+    // socket start()
+    expect(callSocketStart).to.be.equal(true);
+
+    // reset
+    Socket.prototype.start = socketstartFn;
+  })
 
   it('_handleServerJSONMessage')
 
