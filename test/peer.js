1398f875194144a4a254a76f93d6c65cbb98d7f3
hc <hheennrryy@gmail.com>
2013-02-23 03:53:02 -0500

    peer tests

--- test/peer.js
+++ test/peer.js
@@ -1,8 +1,38 @@
+var p;
+
 describe('Peer', function() {
 
-  it('constructor')
+  beforeEach(function() {
+    p = new Peer();
+  });
 
-  it('inherits from EventEmitter')
+  // TODO
+  it('constructor', function() {
+    // Ensures alphanumeric id & key and aborts/return early
+    p = new Peer('123');
+    
+
+    // sets id & calls _init()
+    var init = false;
+    var peerinit = Peer.prototype._init;
+    Peer.prototype._init = function() { init = true }
+    p = new Peer('abc123');
+    expect(p.id).to.be.equal('abc123');
+    expect(init).to.be.equal(true);
+    Peer.prototype._init = peerinit;
+
+    // calls _getId when no id given
+    var getId = false;
+    var peergetId = Peer.prototype._getId;
+    Peer.prototype._getId = function() {getId = true }
+    p = new Peer();
+    expect(getId).to.be.equal(true);
+    Peer.prototype._getId = peergetId;
+  });
+
+  it('inherits from EventEmitter', function() {
+    expect(p instanceof EventEmitter).to.be.equal(true);
+  });
 
   it('_getId')
 
