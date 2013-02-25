0a6ae4c7c43d17bd71dc559fdf7b3ab4241a55d3
hc <hheennrryy@gmail.com>
2013-02-25 01:59:52 -0500

    _startWebSocket test

--- test/socket.js
+++ test/socket.js
@@ -3,7 +3,7 @@ var sock;
 describe('Socket', function() {
 
   beforeEach(function() {
-    sock = new Socket();
+    sock = new Socket('example.com', '80', 'mykey', 'someid');
   })
 
   /*
@@ -11,9 +11,8 @@ describe('Socket', function() {
    *  sets this._httpUrl and this._wsUrl
    */
   it('constructor', function() {
-    sock = new Socket('example.com', 'alphabetPort', 'mykey', 'someid');
-    expect(sock._httpUrl).to.match(/^http:\/\/example\.com:alphabetPort\/mykey\/someid\//);
-    expect(sock._wsUrl).to.match(/^ws:\/\/example\.com:alphabetPort\/peerjs\?key=mykey&id=someid&token=/);
+    expect(sock._httpUrl).to.match(/^http:\/\/example\.com:80\/mykey\/someid\//);
+    expect(sock._wsUrl).to.match(/^ws:\/\/example\.com:80\/peerjs\?key=mykey&id=someid&token=/);
 
     var token1 = sock._wsUrl.substr(sock._wsUrl.indexOf('&token=') + 7);
     var token2 = sock._httpUrl.substr(sock._httpUrl.indexOf('someid/') + 7);
@@ -25,7 +24,7 @@ describe('Socket', function() {
   })
 
   /*
-   *  Starts socket calling _startXhrStream & _startWebSocket
+   *  calls _startXhrStream & _startWebSocket
    */
   it('start', function() {
     var xhrCall = intercept(Socket.prototype, Socket.prototype._startXhrStream);
@@ -35,7 +34,36 @@ describe('Socket', function() {
     expect(!!wsCall()).to.be.equal(true);
   })
 
-  it('_startWebSocket')
+  /*
+   *  creates a WebSocket on ._socket
+   *  (no error handling? que? TODO?)
+   *
+   *  sets event listener onmessage & onopen
+   */
+  it('_startWebSocket', function() {
+    sock._startWebSocket();
+    // calling _startWebSocket again returns early
+    var s = sock._socket;
+    sock._startWebSocket();
+    expect(s).to.be.equal(sock._socket);
+
+    // sets a event listener that relays onmessage
+    var relay = false;
+    sock.addListener('message', function(msg) {
+      expect(msg.txt).to.be.equal('hi');
+      relay = true;
+    });
+    sock._socket.onmessage({data: JSON.stringify({txt: "hi"}) });
+    expect(relay).to.be.equal(true);
+    // PeerJS only handles JSON messages (if not, the expect statement in addListener would throw / fail)
+    sock._socket.onmessage({data: new Blob() });
+
+    // if websocket successful, clear http request stuff
+    sock._socket.onopen();
+
+    // TODO
+
+  })
 
   it('_startXhrStream')
 
